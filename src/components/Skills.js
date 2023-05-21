932e58f87cbed0bfd6e722f5bbccf1f7ae36987d
SHUBHAM SRIVASTAVA <41222490+srivastavshubham@users.noreply.github.com>
2023-05-21 13:22:08 +0530

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -33,13 +33,41 @@ export default function Skills() {
         slidesToShow: 8,
         slidesToScroll: 1
       };
+      const settings1 = {
+        dots: false,
+        autoplay: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      };
 
     return (
         <div id="skills" className="mt-4 text-white">
             <h1 className="text-2xl font-bold">Skills and Technologies</h1>
             <p className="font-light text-gray-400">Here are some of my skills</p>
-             <div className="mt-4">
+             <div className="mt-4 desktop-view">
                 <Slider {...settings}>
+                <SkillCard name="javascript" experience="1 years" img={javascript} />
+                <SkillCard name="react" experience="1 years" img={reactIcon} />
+                <SkillCard name="express" experience="1 years" img={express} />
+                <SkillCard name="nodejs" experience="1 years" img={nodejs} />
+                <SkillCard name="html" experience="1 years" img={html} />
+                <SkillCard name="python" experience="1 years" img={python} />
+                <SkillCard name="gitlab" experience="1 years" img={gitlab} />
+                <SkillCard name="git" experience="1 years" img={git} />
+                <SkillCard name="linux" experience="1 years" img={linux} />
+                <SkillCard name="windows" experience="1 years" img={windows} />
+                <SkillCard name="es6" experience="1 years" img={es6} />
+                <SkillCard name="mongo" experience="1 years" img={mongo} />
+                <SkillCard name="npm" experience="1 years" img={npm} />
+                <SkillCard name="reactnative" experience="1 years" img={reactnative} />
+                <SkillCard name="sql" experience="1 years" img={sql} />
+                <SkillCard name="vscode" experience="1 years" img={vscode} />
+                <SkillCard name="xcode" experience="1 years" img={xcode} />
+                </Slider>
+            </div>
+            <div className="mt-4 mobile-view">
+                <Slider {...settings1}>
                 <SkillCard name="javascript" experience="1 years" img={javascript} />
                 <SkillCard name="react" experience="1 years" img={reactIcon} />
                 <SkillCard name="express" experience="1 years" img={express} />

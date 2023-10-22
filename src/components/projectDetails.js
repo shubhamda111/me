053ec974d436aa08e04b7cc16fadb4b49439cb60
shubhan srivastava <shubhamda111@gmail.com>
2023-10-22 11:50:06 +0530

    add projects

---
+++ src/components/projectDetails.js
@@ -0,0 +1,20 @@
+import explorer from "../assets/certs/explorer.png"
+import landing from "../assets/certs/landing.png"
+import wallet from "../assets/certs/wallet.png"
+import wallet_mob from "../assets/certs/wallet_mob.jpeg"
+import merchant_mob from "../assets/certs/merchnat_mob.jpeg"
+import meme from "../assets/certs/meme.png"
+import shopping from "../assets/certs/shopping.png"
+import weather from "../assets/certs/weather.png"
+
+export const projectDetails=[
+    {project_url:"https://explorer.ovato.com",image:explorer,project_name:"Ovato Blockchain Explorer",project_tech:"React, Node, Express & Mongo"},
+    {project_url:"https://wallet.ovato.com",image:wallet,project_name:"Ovato Web Wallet",project_tech:"React, Node"},
+    {project_url:"https://ovato.com/en",image:landing,project_name:"Ovato",project_tech:"React, Node"},
+    {project_url:"https://play.google.com/store/apps/details?id=com.ovato.wallet&hl=en&gl=US",image:wallet_mob,project_name:"Ovato Wallet App - Android",project_tech:"React Native, FCM, Android Studio"},
+    {project_url:"https://play.google.com/store/apps/details?id=com.merchantwalletapp&hl=en&gl=US&pli=1",image:merchant_mob,project_name:"Ovato Merchant App - Android",project_tech:"React Native, FCM, Android Studio"},
+    {project_url:"https://github.com/srivastavshubham/React-Create-Memes",image:meme,project_name:"Create Meme's",project_tech:"React"},
+    {project_url:"https://github.com/srivastavshubham/shopping-app",image:shopping,project_name:"Shopping Web",project_tech:"React, Context"},
+    {project_url:"https://srivastavshubham.github.io/weather-app/",image:weather,project_name:"Weather App",project_tech:"React"},
+
+]

33afe0a0cdf8611ca3db7645268b00ad5ce111f3
shubhan srivastava <shubhamda111@gmail.com>
2023-12-11 14:43:03 +0530

    add project

--- src/components/projectDetails.js
+++ src/components/projectDetails.js
@@ -6,6 +6,8 @@ import merchant_mob from "../assets/images/merchnat_mob.jpeg"
 import meme from "../assets/images/meme.png"
 import shopping from "../assets/images/shopping.png"
 import weather from "../assets/images/weather.png"
+import spotify from "../assets/images/spotify.png"
+
 
 export const projectDetails=[
     {project_url:"https://explorer.ovato.com",image:explorer,project_name:"Ovato Blockchain Explorer",project_tech:"React, Node, Blockchain, Express & Mongo"},
@@ -14,7 +16,8 @@ export const projectDetails=[
     {project_url:"https://play.google.com/store/apps/details?id=com.ovato.wallet&hl=en&gl=US",image:wallet_mob,project_name:"Ovato Wallet App - Android",project_tech:"React Native, FCM, Android Studio"},
     {project_url:"https://play.google.com/store/apps/details?id=com.merchantwalletapp&hl=en&gl=US&pli=1",image:merchant_mob,project_name:"Ovato Merchant App - Android",project_tech:"React Native, FCM, Android Studio"},
     {project_url:"https://github.com/srivastavshubham/React-Create-Memes",image:meme,project_name:"Create Meme's",project_tech:"React"},
-    {project_url:"https://github.com/srivastavshubham/shopping-app",image:shopping,project_name:"Shopping Web",project_tech:"React, Context"},
+    {project_url:"https://srivastavshubham.github.io/shopping-app/",image:shopping,project_name:"Shopping Web",project_tech:"React, Context"},
     {project_url:"https://srivastavshubham.github.io/weather-app/",image:weather,project_name:"Weather App",project_tech:"React"},
+    {project_url:"https://github.com/srivastavshubham/spotify/tree/master",image:spotify,project_name:"Spotify Clone",project_tech:"React,Context,Tailwind,Spotify API"},
 
 ]
